b8c742e0903e0b1561c8e60329268117ecba16cb
sensebbx <33457416+sensebbx@users.noreply.github.com>
2017-11-09 00:10:00 +0300

    Add files via upload
    Linked Parser part and Interface part.

--- Interface/Interface/MainWindow.xaml.cs
+++ Interface/Interface/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using PcapParser;
 
 namespace Interface
 {
@@ -27,11 +28,6 @@ namespace Interface
             InitializeComponent();
         }
 
-        private void FEButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
         private void BtnOpenFile_OnClick(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog {Filter = "PCAP-files (*.pcap)|*.pcap"};
@@ -40,9 +36,48 @@ namespace Interface
                 txtEditor.Text = openFileDialog.FileName;
         }
 
-        private void BtnParce_OnClick(object sender, RoutedEventArgs e)
+	    public class Item
+	    {
+		    public string ColOne { get; set; }
+			public string ColTwo { get; set; }
+			public string ColThree { get; set; }
+		}
+
+        private void BtnParse_OnClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Developing", "Coming soon");
+	        if (txtEditor.Text == "")
+	        {
+		        MessageBox.Show("Device path error", "Error");
+				return;
+	        }
+
+	        dgTable.ColumnWidth = 406;
+	        dgTable.CanUserResizeColumns = false;
+	        dgTable.AutoGenerateColumns = false;
+
+			PcapParser.PacketManipulation parser = new PacketManipulation();
+	        parser.Parse(txtEditor.Text);
+	        var table = parser.pcapTable;
+
+			DataGridTextColumn columnOne = new DataGridTextColumn();
+	        columnOne.Header = "Protocol type";
+	        columnOne.Binding = new Binding("ColOne");
+			dgTable.Columns.Add(columnOne);
+
+			DataGridTextColumn columnTwo = new DataGridTextColumn();
+			columnTwo.Header = "Source address";
+			columnTwo.Binding = new Binding("ColTwo");
+			dgTable.Columns.Add(columnTwo);
+
+			DataGridTextColumn columnThree = new DataGridTextColumn();
+			columnThree.Header = "Destination address";
+			columnThree.Binding = new Binding("ColThree");
+			dgTable.Columns.Add(columnThree);
+
+			foreach (var raw in table)
+	        {
+		        dgTable.Items.Add(new Item() {ColOne = raw[0], ColTwo = raw[1], ColThree = raw[2]});
+	        }
         }
     }
 }
