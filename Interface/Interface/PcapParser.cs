a39b69c3079e33fecb324fdc5e41d343041c4990
HarshRatel <bazarovmsk@yandex.ru>
2017-11-21 23:02:19 +0300

    update 21.11.2017
    + Added logging
    + Added info field

--- Interface/Interface/PcapParser.cs
+++ Interface/Interface/PcapParser.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Net.NetworkInformation;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Net;
-using System.Windows.Documents;
 using PacketDotNet;
 using SharpPcap;
 using SharpPcap.LibPcap;
@@ -14,7 +9,7 @@ namespace PcapParser
 	public class PacketManipulation
 	{
 		public List<List<string>> pcapTable { get; set; }
-
+	    public Logger logger = new Logger();
 		public PacketManipulation() { pcapTable = new List<List<string>>();}
 
 		public void Parse(string devicePath)
@@ -42,49 +37,50 @@ namespace PcapParser
 				var eth = ((PacketDotNet.EthernetPacket)packet);
 
 				var ip = (PacketDotNet.IpPacket)packet.Extract(typeof(PacketDotNet.IpPacket));
-				if (ip != null)
-				{
-					var tcp = (PacketDotNet.TcpPacket)packet.Extract(typeof(PacketDotNet.TcpPacket));
-					if (tcp != null)
-					{
-						row.Add("TCP");
-                        row.Add(time.ToString("s.ffff"));
-						row.Add(ip.SourceAddress.ToString());
-						row.Add(ip.DestinationAddress.ToString());
-                        row.Add(len.ToString());
-                        row.Add(tcp.SourcePort + " -> " + tcp.DestinationPort + "  Seq: " + tcp.SequenceNumber);
-					    tcp.Header.ToString();
-						pcapTable.Add(row);
-					}
+			    if (ip != null)
+			    {
+			        var tcp = (PacketDotNet.TcpPacket) packet.Extract(typeof (PacketDotNet.TcpPacket));
+			        if (tcp != null)
+			        {
+			            row.Add("TCP");
+			            row.Add(time.ToString("s.ffff"));
+			            row.Add(ip.SourceAddress.ToString());
+			            row.Add(ip.DestinationAddress.ToString());
+			            row.Add(len.ToString());
+			            row.Add(tcp.SourcePort + " -> " + tcp.DestinationPort + "  Seq: " + tcp.SequenceNumber + " Win " + tcp.WindowSize);
 
-					var udp = (PacketDotNet.UdpPacket)packet.Extract(typeof(PacketDotNet.UdpPacket));
-					if (udp != null)
-					{
-                        row.Add("UDP");
-                        row.Add(time.ToString("s.ffff"));
-                        row.Add(ip.SourceAddress.ToString());
-                        row.Add(ip.DestinationAddress.ToString());
-                        row.Add(len.ToString());
-                        row.Add(udp.SourcePort + "->" + udp.DestinationPort);
-                        
-                        pcapTable.Add(row);
-					}
+			            pcapTable.Add(row);
+			        }
 
-				    var icmp = (PacketDotNet.ICMPv4Packet) packet.Extract((typeof (PacketDotNet.ICMPv4Packet)));
-				    if (icmp != null)
-				    {
-				        row.Add("ICMP");
-                        row.Add(time.ToString("s.ffff"));
-                        row.Add(ip.SourceAddress.ToString());
-                        row.Add(ip.DestinationAddress.ToString());
-                        row.Add(len.ToString());
-                        row.Add("id: " + icmp.ID);
-                        
+			        var udp = (PacketDotNet.UdpPacket) packet.Extract(typeof (PacketDotNet.UdpPacket));
+			        if (udp != null)
+			        {
+			            row.Add("UDP");
+			            row.Add(time.ToString("s.ffff"));
+			            row.Add(ip.SourceAddress.ToString());
+			            row.Add(ip.DestinationAddress.ToString());
+			            row.Add(len.ToString());
+			            row.Add(udp.SourcePort + "->" + udp.DestinationPort);
 
-                        pcapTable.Add(row);
-				    }
-                    
-				}
+			            pcapTable.Add(row);
+			        }
+
+			        var icmp = (PacketDotNet.ICMPv4Packet) packet.Extract((typeof (PacketDotNet.ICMPv4Packet)));
+			        if (icmp != null)
+			        {
+			            row.Add("ICMP");
+			            row.Add(time.ToString("s.ffff"));
+			            row.Add(ip.SourceAddress.ToString());
+			            row.Add(ip.DestinationAddress.ToString());
+			            row.Add(len.ToString());
+			            row.Add("id: " + icmp.ID);
+			        }
+
+			    }
+			    else
+			    {
+			        logger.CommonLog("failed to read ip packet");
+			    }
 			}
 		}
 	}
