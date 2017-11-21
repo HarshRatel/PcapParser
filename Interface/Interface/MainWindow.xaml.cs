df867017c942143033b84bccd5de9fb2befb23e0
sensebbx <samnikser@gmail.com>
2017-11-22 00:58:58 +0300

    Updates and bug fixes
    *Added filter functions
    *Changed table logical structure
    *Fixed bug with fake "Parse" button click
    *Fixed double click on table bug

--- Interface/Interface/MainWindow.xaml.cs
+++ Interface/Interface/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,39 +19,45 @@ using PcapParser;
 
 namespace Interface
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			_parcedList = new List<List<string>>();
+			_myCollection = new ObservableCollection<Item>();
+			dgTable.ItemsSource = _myCollection;
+		}
+
+		private static List<List<string>> _parcedList;
+		private ObservableCollection<Item> _myCollection;
 
 		/// <summary>
 		/// "btnOpenFile" button click handler
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-        private void BtnOpenFile_OnClick(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog {Filter = "PCAP-files (*.pcap)|*.pcap"};
+		private void BtnOpenFile_OnClick(object sender, RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog {Filter = "PCAP-files (*.pcap)|*.pcap"};
 
-            if (openFileDialog.ShowDialog() == true)
-                txtEditor.Text = openFileDialog.FileName;
-        }
+			if (openFileDialog.ShowDialog() == true)
+				txtEditor.Text = openFileDialog.FileName;
+		}
 
 		/// <summary>
 		/// Columns class
 		/// </summary>
-	    private class Item
+		private class Item
 		{
 			public string protocolColumn { get; set; }
 			public string timeColumn { get; set; }
 			public string srcColumn { get; set; }
 			public string dstColumn { get; set; }
-            public string lengthColumn { get; set; }
+			public string lengthColumn { get; set; }
 			public string infoColumn { get; set; }
 		}
 
@@ -59,86 +66,131 @@ namespace Interface
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-        private void BtnParse_OnClick(object sender, RoutedEventArgs e)
-        {
-	        if (txtEditor.Text == "")
-	        {
-		        MessageBox.Show("Device path error", "Error");
+		private void BtnParse_OnClick(object sender, RoutedEventArgs e)
+		{
+			if (txtEditor.Text == "" || txtEditor.Text == "Device path")
+			{
+				MessageBox.Show("Device path error", "Error");
 				return;
-	        }
-
-			dgTable.Columns.Clear();
-
-	        dgTable.ColumnWidth = 203;
-	        dgTable.CanUserResizeColumns = false;
-	        dgTable.AutoGenerateColumns = false;
+			}
 
 			var parser = new PacketManipulation();
-	        parser.Parse(txtEditor.Text);
-	        var table = parser.pcapTable;
-			
-			var protocolColumn = new DataGridTextColumn
+			parser.Parse(txtEditor.Text);
+			_parcedList.Clear();
+			_parcedList = parser.pcapTable;
+
+			DrawTable(_parcedList);
+		}
+
+		/// <summary>
+		/// Displays table from list
+		/// </summary>
+		/// <param name="table">List of table</param>
+		private void DrawTable(List<List<string>> table)
+		{
+			if (table.Count == 0)
+			{
+				return;
+			}
+
+			_myCollection.Clear();
+
+			/*var protocolColumn = new DataGridTextColumn
 			{
 				Header = "Protocol type",
-				Binding = new Binding("protocolColumn")
+				Binding = new Binding("protocolColumn"),
+				Width = 140
 			};
 			dgTable.Columns.Add(protocolColumn);
 
 			var timeColumn = new DataGridTextColumn
 			{
 				Header = "Time",
-				Binding = new Binding("timeColumn")
+				Binding = new Binding("timeColumn"),
+				Width = 125
 			};
 			dgTable.Columns.Add(timeColumn);
 
 			var srcColumn = new DataGridTextColumn
 			{
 				Header = "Source IP",
-				Binding = new Binding("srcColumn")
+				Binding = new Binding("srcColumn"),
+				Width = 125
 			};
 			dgTable.Columns.Add(srcColumn);
 
 			var dstColumn = new DataGridTextColumn
 			{
 				Header = "Destination IP",
-				Binding = new Binding("dstColumn")
+				Binding = new Binding("dstColumn"),
+				Width = 125
 			};
 			dgTable.Columns.Add(dstColumn);
 
 			var lengthColumn = new DataGridTextColumn
 			{
 				Header = "Length",
-				Binding = new Binding("lengthColumn")
+				Binding = new Binding("lengthColumn"),
+				Width = 60
 			};
 			dgTable.Columns.Add(lengthColumn);
 
-	        var infoColumn = new DataGridTextColumn
-	        {
+			var infoColumn = new DataGridTextColumn
+			{
 				Header = "Info",
-				Binding = new Binding("infoColumn")
-	        };
-			dgTable.Columns.Add(infoColumn);
+				Binding = new Binding("infoColumn"),
+				Width = 644
+			};
+			dgTable.Columns.Add(infoColumn);*/
 
 			foreach (var raw in table)
-	        {
-                dgTable.Items.Add(new Item() { protocolColumn = raw[0],
-												timeColumn  = raw[1],
-												srcColumn = raw[2],
-												dstColumn = raw[3],
-												lengthColumn = raw[4],
-												infoColumn = raw[5]
-												});
-	        }
-        }
+			{
+				_myCollection.Add(new Item()
+				{
+					protocolColumn = raw[0],
+					timeColumn = raw[1],
+					srcColumn = raw[2],
+					dstColumn = raw[3],
+					lengthColumn = raw[4],
+					infoColumn = raw[5]
+				});
+			}
+		}
 
 		/// <summary>
 		/// "btnLog" button handler
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-	    private void BtnLog_OnClick(object sender, RoutedEventArgs e)
-	    {
+		private void BtnLog_OnClick(object sender, RoutedEventArgs e)
+		{
 			System.Diagnostics.Process.Start(System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"../../../logs/logs.txt"));
 		}
-    }
+
+		/// <summary>
+		/// "dgTable" editing stub
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void DgTable_OnBeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+		{
+			e.Cancel = true;
+		}
+
+		/// <summary>
+		/// Filter handler
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void TbFilter_OnTextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (tbFilter.Text.Length == 0)
+			{
+				DrawTable(_parcedList);
+				return;
+			}
+			var filteredList = _parcedList.Where(raw => raw[0].Contains(tbFilter.Text)).ToList();
+			DrawTable(filteredList);
+		}
+	}
 }
